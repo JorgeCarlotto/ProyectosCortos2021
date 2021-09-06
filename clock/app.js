e5ae3bbf950755266a9825d7f6cbb4c6c05bae2c
JorgeCarlotto <jorgemcarlotto@gmail.com>
2021-09-06 18:37:30 -0300

    agregado funcion alarma

--- clock/app.js
+++ clock/app.js
@@ -18,5 +18,30 @@ d.addEventListener("click",(e)=>{
         d.querySelector(actClok).disabled = false;
 }
 })    
+//alarm
+const actAlarm = "#activate-alarm"
+const dactAlarm = "#deactivate-alarm"
+const sound = "assets/alarma.mp3"
 
+let alarmaTempo;
+const $alarm = d.createElement("audio"); 
+$alarm.src= sound; 
 
+d.addEventListener("click",(e)=>{
+    if(e.target.matches(actAlarm)){
+        alarmaTempo = setTimeout(()=>{
+            $alarm.play();
+        },1000)
+
+        e.target.disabled = true; 
+
+    }
+    if(e.target.matches(dactAlarm)){
+        clearTimeout(alarmaTempo);
+        $alarm.pause(); 
+        $alarm.currentTime = 0;
+        d.querySelector(actAlarm ).disabled = false; 
+
+    }
+
+})
